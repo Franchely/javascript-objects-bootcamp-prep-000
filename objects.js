89cedc6855ab8f54c6f542ec379d0eade2fccb7d
Franchely Portorreal <franchely811@gmail.com>
2019-05-03 16:23:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 var playlist = new Object ({ "Slowdive": "Alison", "My Bloody Valentine": "Sometimes"}); 
 function updatePlaylist(playlist, artistName, songTitle) {
- return Object.assign(playlist, {["Phil Ochs"]: "Here's to the State of Mississippi"}) 
+ return Object.assign(playlist, {[artistName]: songTitle}) 
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist. 
+  delete playlist.artistName 
+  return playlist 
 }
